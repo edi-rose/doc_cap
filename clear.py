1d8a1001aec144b3bf67d4521c5c3be6d8e2239a
Edi Rose <edi.rose@strategyblocks.com>
2022-06-17 10:38:31 +1200

    added clear pdfs to clear.py

--- clear.py
+++ clear.py
@@ -5,3 +5,8 @@ def clear_pngs():
     for file in os.listdir('./images/'):
         if file.endswith('.png'):
             os.remove('./images/'+file) 
+
+def clear_pdfs():
+    for file in os.listdir('./images/'):
+        if file.endswith('.pdf'):
+            os.remove('./images/'+file) 
