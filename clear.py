0e002641131594ebde64176e22c6fdcb6e94e129
Edi Rose <edi.rose@strategyblocks.com>
2023-03-07 13:20:20 +1300

    added conditions to clear functions

--- clear.py
+++ clear.py
@@ -2,11 +2,15 @@
 import os   
 # removes all the images in the folder
 def clear_pngs():
-    for file in os.listdir('./images/'):
-        if file.endswith('.png'):
-            os.remove('./images/'+file) 
+    for x in os.listdir('./images/'):
+        if not x.startswith('.') and not x.startswith('1'):
+            for files in os.listdir('./images/'+x):
+                if files.endswith('.png'):
+                    os.remove('./images/'+ x + '/' + files) 
 
 def clear_pdfs():
-    for file in os.listdir('./images/'):
-        if file.endswith('.pdf'):
-            os.remove('./images/'+file) 
+    for subdirs in os.listdir('./images/'):
+        if not subdirs.startswith('.') and not subdirs.startswith('1'):
+            for files in os.listdir('./images/'+subdirs):
+                if files.endswith('.pdf'):
+                    os.remove('./images/'+subdirs + '/' +files) 
